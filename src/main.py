f2383c3cb91a8c5ba4a787fbb9c2da1979ca3ada
Masoma Shoayb <masomashoayb@Masomas-MBP.lan>
2026-08-30 16:05:58 -0400

    Add project files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 def welcome_message(name):
-    return f"{name}, welcome to the Data Engineering course."
+    name=name.strip()
+    if not name:
+        name='Guest_student'
+
+    return f"Welcome aboard, {name}! Let's dive into the world of Data Engineering."
 
 
 if __name__ == "__main__":
     name = input("Enter your name: ")
     print(welcome_message(name))
+    
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,7 @@
 from src.main import welcome_message
 
 def test_welcome_message():
-    assert welcome_message("Ammy") == "Ammy, welcome to the Data Engineering course."
+    assert welcome_message("Ammy") == "Welcome aboard, Ammy! Let's dive into the world of Data Engineering."
+
+def test_welcome_message_empty():
+    assert welcome_message("") == "Welcome aboard, Guest_student! Let's dive into the world of Data Engineering."
